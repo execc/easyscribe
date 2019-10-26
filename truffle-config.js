d937f3ee731b57dff5c5b6acbf1256b4dc6a3eef
dvasin <dvasin@wavesplatform.com>
2019-10-26 12:39:07 +0300

    Kovan works

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const KVN_SEED = "need raven outside attitude dwarf agree average canvas wood snack penalty position"
+const KVN_URL = "https://kovan.infura.io/v3/5b52483999bb42d6adb222571326568d"
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +11,11 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    kovan: {
+      provider: () => new HDWalletProvider(KVN_SEED, KVN_URL),
+      network_id: 42,
+      gas: 4700000
     }
   }
 };
