bfdc2e0826f223283d66bc4f5f1e2dce847d7be6
Nekrasov Alexey <avnekr@yandex.ru>
2019-10-26 23:38:34 +0300

    ux improve sub form

--- client/src/modules/ConnectForm/ConnectForm.tsx
+++ client/src/modules/ConnectForm/ConnectForm.tsx
@@ -23,8 +23,20 @@ type OwnProps = {
 
 type Props = OwnProps & FormComponentProps;
 
-class ConnectForm extends React.Component<Props> {
+type State = {
+  processing: boolean;
+};
+
+class ConnectForm extends React.Component<Props, State> {
+  state: State = {
+    processing: false,
+  };
+
   handleSubscribe = async () => {
+    this.setState({
+      processing: true,
+    });
+
     try {
       const {
         web3,
@@ -69,8 +81,17 @@ class ConnectForm extends React.Component<Props> {
           periodCount
         )
         .send({ from: this.props.account });
+
+      message.success("You successfully subscribed");
+
+      this.setState({
+        processing: false,
+      });
     } catch (error) {
-      message.error("Произошла ошибка подписке");
+      this.setState({
+        processing: false,
+      });
+      message.error("Subscription process ends with error");
       console.error(error);
     }
   };
@@ -85,11 +106,57 @@ class ConnectForm extends React.Component<Props> {
     });
   };
 
+  renderAmount = () => {
+    const {
+      config: { amount },
+    } = this.props;
+
+    return (
+      <div>
+        Amount:{" "}
+        {typeof amount === "undefined"
+          ? null
+          : Number(amount) / Math.pow(10, 18)}
+      </div>
+    );
+  };
+
+  renderPeriod = () => {
+    const {
+      config: { period },
+    } = this.props;
+
+    return (
+      <div>
+        Period: {typeof period === "undefined" ? null : Number(period) / 60}
+      </div>
+    );
+  };
+
+  renderPeriodCount = () => {
+    const {
+      config: { periodCount },
+    } = this.props;
+
+    return <div>Period count: {periodCount}</div>;
+  };
+
+  renderPaymentInfo = () => {
+    return (
+      <>
+        {this.renderAmount()}
+        {this.renderPeriod()}
+        {this.renderPeriodCount()}
+      </>
+    );
+  };
+
   renderPaymentMethods = () => {
     const {
       form: { getFieldDecorator },
       config: { paymentMethods },
     } = this.props;
+    const { processing } = this.state;
 
     if (!paymentMethods) {
       return null;
@@ -97,8 +164,14 @@ class ConnectForm extends React.Component<Props> {
 
     return (
       <Form.Item label="Payment method">
-        {getFieldDecorator("paymentMethod")(
-          <Select placeholder="Payment method" defaultActiveFirstOption>
+        {getFieldDecorator("paymentMethod", {
+          rules: [{ required: true, message: "Please choose payment method" }],
+        })(
+          <Select
+            placeholder="Payment method"
+            defaultActiveFirstOption
+            disabled={processing}
+          >
             {paymentMethods.map(method => (
               <Select.Option key={method.value}>{method.title}</Select.Option>
             ))}
@@ -109,14 +182,32 @@ class ConnectForm extends React.Component<Props> {
   };
 
   render() {
+    const { processing } = this.state;
+
     return (
       <Form className="subscribe-form" onSubmit={this.handleSubmit}>
         <div className="subscribe-form-header">Subscription</div>
+        {this.renderPaymentInfo()}
         {this.renderPaymentMethods()}
         <Form.Item className="subscribe-form-footer">
-          <Button type="primary" htmlType="submit">
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={processing}
+            loading={processing}
+            className="subscribe-button"
+          >
             Subscribe
           </Button>
+          <div className="additional-info">
+            You can cancel the subscription in any time and receive you money
+            back!
+            <br />
+            Manage you subscriptions on{" "}
+            <a href="http://127.0.0.1:3000/#/subscriptions">
+              subscriptions panel
+            </a>
+          </div>
         </Form.Item>
       </Form>
     );
