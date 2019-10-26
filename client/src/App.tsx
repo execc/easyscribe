0dccad49559d66e0210490fce76a93046ce57340
Nekrasov Alexey <avnekr@yandex.ru>
2019-10-26 14:03:05 +0300

    app stub

---
+++ client/src/App.tsx
@@ -0,0 +1,54 @@
+import { message, Spin } from "antd";
+import React, { Component } from "react";
+import getWeb3 from "./utils/getWeb3";
+
+import "./App.css";
+import "antd/dist/antd.css";
+
+type State = {
+  account: any;
+};
+
+class App extends Component<{}, State> {
+  web3: any;
+
+  state: State = { account: null };
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+
+      if (networkId !== 42) {
+        throw new Error(`networkId: ${networkId}`);
+      }
+
+      this.web3 = web3;
+      this.setState({ account: accounts[0] });
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      message.error("Произошла ошибка при инициализации приложения");
+      console.error(error);
+    }
+  };
+
+  render() {
+    const { account } = this.state;
+
+    if (!account) {
+      return <div className="spin-wrapper">
+        <Spin size="large"/>
+      </div>;
+    }
+
+    return <div className="App">hallo</div>;
+  }
+}
+
+export { App };
