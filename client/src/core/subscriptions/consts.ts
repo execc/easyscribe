81801182d9779e06f612a9842a1e6532ecbd935d
Nekrasov Alexey <avnekr@yandex.ru>
2019-10-27 11:10:46 +0300

    add service name mapping

--- client/src/core/subscriptions/consts.ts
+++ client/src/core/subscriptions/consts.ts
@@ -1,4 +1,4 @@
-import {OptionProps} from "antd/es/select";
+import { OptionProps } from "antd/es/select";
 
 export enum SubscriptionStatus {
   ACTIVE = "ACTIVE",
@@ -28,3 +28,16 @@ export const paymentMethodOptions: OptionProps[] = [
     title: "DAI",
   },
 ];
+
+export const getServiceNames = (address: string): string => {
+    switch (address.toLocaleLowerCase()) {
+        case "0x7dB647031EE753604CC3aE49592de4C09818f23b".toLocaleLowerCase():
+            return "Yandex.Music";
+        case "0x8D933D915Ae4f74D1b5BA32466c5676F2E15E5A1".toLocaleLowerCase():
+            return "Apple.Music";
+        case "0x5C614f3913a381cF74cBA2F55d57902EC60CC4F7".toLocaleLowerCase():
+            return "Google.Music";
+        default:
+            return "";
+    }
+};
