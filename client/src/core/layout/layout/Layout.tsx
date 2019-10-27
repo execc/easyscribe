21eaa3d224505a086e0e7739910830fd329074b1
Nekrasov Alexey <avnekr@yandex.ru>
2019-10-27 08:38:12 +0300

    app layout with side menu

---
+++ client/src/core/layout/layout/Layout.tsx
@@ -0,0 +1,63 @@
+import { Layout, Menu, Icon } from "antd";
+import React from "react";
+import {Link} from "react-router-dom";
+import "./Layout.css";
+
+const { Sider, Content } = Layout;
+
+class CustomLayout extends React.Component {
+  getMenuConfig = () => {
+    return [
+      {
+        value: "subscriptions",
+        title: "Subscriptions",
+        icon: "container",
+      },
+      {
+        value: "subscribers",
+        title: "Subscribers",
+        icon: "api",
+      },
+      {
+        value: "market",
+        title: "Market",
+        icon: "shopping-cart",
+      },
+    ];
+  };
+
+  renderSider = () => {
+    return (
+      <Sider width={200} style={{ background: "#fff" }}>
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={[location.hash.substr(2)]}
+          style={{ height: "100%", borderRight: 0 }}
+        >
+          {this.getMenuConfig().map(menuItem => (
+            <Menu.Item key={menuItem.value}>
+              <Link to={menuItem.value}>
+                <Icon type={menuItem.icon} />
+                <span>{menuItem.title}</span>
+              </Link>
+            </Menu.Item>
+          ))}
+        </Menu>
+      </Sider>
+    );
+  };
+
+  render() {
+    return (
+      <Layout>
+        {/*{this.renderHeader()}*/}
+        <Layout>
+          {this.renderSider()}
+          <Content>{this.props.children}</Content>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+export { CustomLayout as Layout };
